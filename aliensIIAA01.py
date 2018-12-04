a4d14a84f7cc5ba7e8e249cb80038591da299e77
csPython <cspython@AD25TF04NGF1J-114.local>
2018-12-04 13:39:54 -0500

    second version of code

--- aliensIIAA01.py
+++ aliensIIAA01.py
@@ -3,22 +3,28 @@ l = int(0) # number of aliens that land originally
 w = int(0) # number of weeks the sliens have been on earth 
 p = int(0) # the population of aliens based on the users imput
 wkDisp = str("week")
+checkL = False
 
 # introduction
 print(" Welcome to your Alien counter where we determine the population of aliens ")
 print("based on the inputs for the number of aliens landed ")
 print("and how long they've been on earth that you type.")
 
-week = False
-while week == false:
-    try:
-        w = int(input ("[Please Give a Positive Integer] "))
 #Q1
 print()
 print ("How many aliens have landed on earth")
-l = int(input ("[ Please Give a Positive Integer] "))
-if l > 0:
-    print()
+
+while checkL == False:
+    try:
+        l = int(input ("[ Please Give a Positive Integer] "))
+        if l > 0:
+            print("Good ANSWER")
+            checkL = True
+        else:
+            print("That wasn't a good answer")
+            
+    except ValueError:
+        print("Maybe try somthing like a... Whole Number.") 
     
    #Q2
     print("How long have the aliens been on earth (This Will Be Determined In Weeks)")
